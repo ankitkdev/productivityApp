3276e6d623e48f473f59f2d372804dc784ddc9b6
BodduKranti <bkranti.kumar2@gmail.com>
2022-11-23 12:11:51 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./Components/Header";
 import Todoform from "./Components/Todoform";
+import './App.css';
 
 function App() {
   return (
     <>
       
-      <div className="container h-100 my-5 d-flex justify-content-center align-content-center">
+      <div className="container h-100 my-5 d-flex justify-content-center align-items-center">
         <div>
         <Header />
         <Todoform />
--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -1,14 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Todoform = () => {
-  return (
-    <>
-        <form className='d-flex'>
-            <input className='form-control' name='todo' placeholder="Enter the todo name" />
-            <button className='btn btn-danger'>Add Todo</button>    
-        </form>            
-    </>
-  )
+
+    const [todoField, setTodoField] = useState('');
+    const [storeTodo, setStoreTodo] = useState([]);
+    const [error,setError]=useState('')
+
+    const addTodo = (e) => {
+        e.preventDefault();
+        if(todoField===''){
+            setError('Please do not leave blank');
+        }
+        else{
+            setStoreTodo([...storeTodo, todoField]);
+            setError('');
+        }
+        
+    }
+
+    console.log(storeTodo)
+    return (
+        <>
+        {error?<>{error}</>:<></> }
+            <form className='d-flex'>
+                <input className='form-control' name='todoField' value={todoField} onChange={(e) => setTodoField(e.target.value)} placeholder="Enter the todo name" />
+                <button className='btn btn-danger col-3' onClick={addTodo}>Add Todo</button>
+            </form>
+
+            {storeTodo && storeTodo.map((itms) => {
+                return (
+                    <>
+                        <div className='form-group py-2'>
+                            <input type="checkbox" /> {itms}
+                        </div>
+                    </>
+                )
+
+            })}
+        </>
+    )
 }
 
 export default Todoform
