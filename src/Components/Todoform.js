2e7c7f930f77ba644cc6fc197ef999c11ae8f56c
BodduKranti <bkranti.kumar2@gmail.com>
2022-11-24 12:00:31 +0530

    todolist,todoform

--- src/Components/Todoform.js
+++ src/Components/Todoform.js
@@ -3,34 +3,48 @@ import Todolist from './Todolist';
 
 const Todoform = () => {
 
-    const [todoField, setTodoField] = useState('');
+    const [todoField, setTodoField] = useState({
+        todoName:'',
+        id:Math.floor(Math.random()*1000),
+        complete:false
+    });
+
+    const {todoName} = todoField;
     const [storeTodo, setStoreTodo] = useState([]);
     const [error,setError]=useState('')
 
+    const todoHandler = (e) =>{
+        setTodoField({...todoField,[e.target.name]:e.target.value,id:Math.floor(Math.random()*1000),complete:false})
+    }
+
     const addTodo = (e) => {
         e.preventDefault();
-        if(todoField===''){
+        if(todoName===''){
             setError('Please do not leave blank');
         }
         else{
-            setStoreTodo([...storeTodo, todoField ]);
+            setStoreTodo([...storeTodo, todoField] );
             setError('');
-            setTodoField('')
+            setTodoField({
+                todoName:''
+            })
         }
         
     }
 
+
     return (
         <>
-        {error?<>{error}</>:<></> }
+                {error?<>{error}</>:<></> }
             <form className='d-flex'>
-                <input className='form-control' name='todoField' value={todoField} onChange={(e) => setTodoField(e.target.value)} placeholder="Enter the todo name" />
+                <input className='form-control' name='todoName' value={todoName} onChange={todoHandler} placeholder="Enter the todo name" />
                 <button className='btn btn-danger col-3' onClick={addTodo}>Add Todo</button>
             </form>
 
             <Todolist 
                 storeTodo={storeTodo}
             />
+            
         </>
     )
 }
--- src/Components/Todolist.js
+++ src/Components/Todolist.js
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Todolist.css'
 
 const Todolist = (props) => {
-   
-   
+
+    console.log(props.storeTodo)
+    
+
     return (
         <>
-            {props.storeTodo && props.storeTodo.map((itms, i) => {
-                return (
-                    <>
+            {props.storeTodo ? <>
+                {props.storeTodo.map((itms, i) => {
+                    return (
+                        <>
 
-                        <div class="form-check col-12 py-2 alert alert-dark mt-2" for="alert" key={i}>
-                            <input class="form-check-input" type="checkbox" value="" id={`todoCheck-${i}`} />
-                            <label class="form-check-label" for={`todoCheck-${i}`}>
-                                {itms}
-                            </label>
-                        </div>
-                    </>
-                )
+                            <div 
+                                className={`form-check col-12 py-2 alert alert-dark mt-2 
+                                    
+                                `} 
+                            
+                                htmlFor="alert" key={i}
+                                
+                            >
+                                <input className="form-check-input" type="checkbox" value="" id={`todoCheck-${i}`} />
+                                <label className="form-check-label" for={`todoCheck-${i}`}>
+                                    {itms.todoName}
+                                </label>
+                            </div>
+                        </>
+                    )
 
-            })}
+                })}
+            </> : ''}
         </>
     )
 }
